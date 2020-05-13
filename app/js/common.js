3d2f1a311586c621e8f8680439d801809108b612
Anna <anna.kuhir@nure.ua>
2020-05-13 20:02:41 +0300

    added edit function

--- app/js/common.js
+++ app/js/common.js
@@ -32,12 +32,15 @@ const renderItemList = (list) => {
 	itemsContainer.innerHTML = '';
 	itemsContainer.innerHTML = `
 	${list.map((item, id) => {
-		return fillItemTemplate(item, id)
+		if (item.isEditing){
+			return createEditTemplate(item, id);
+		}
+		return createBaseItemTemplate(item, id);
 	}).join('')}
 	`
 }
 
-const fillItemTemplate = (item, id) => {
+const createBaseItemTemplate = (item, id) => {
 	// debugger;
 	const itemContainer = document.createElement('div');
 	const itemTitle = document.createElement('h1');
@@ -84,6 +87,77 @@ const fillItemTemplate = (item, id) => {
 	doneButton.classList.add('js-done-button', 'item-functional-button');
 	doneButton.innerText = 'Done';
 	doneButton.id = `done-${id}`;
+
+	statusElement.className = 'status-element';
+	statusElement.innerText = StatusEnam[item.status];
+
+
+	if (item.status === StatusEnam.done) {
+		editButton.classList.add('disabled-button');
+		editButton.disabled = 'true';
+
+		holdButton.classList.add('disabled-button');
+		holdButton.disabled = 'true';
+
+		doneButton.classList.add('disabled-button');
+		doneButton.disabled = 'true';
+	}
+
+	if (item.status === StatusEnam.hold) {
+		editButton.classList.add('disabled-button');
+		editButton.disabled = 'true';
+
+		deleteButton.classList.add('disabled-button');
+		deleteButton.disabled = 'true';
+
+
+		doneButton.classList.add('disabled-button');
+		doneButton.disabled = 'true';
+
+		holdButton.innerText = 'Unhold';
+
+	}
+	return itemContainer.outerHTML;
+
+}
+
+const createEditTemplate = (item, id) => {
+	debugger;
+	const itemContainer = document.createElement('div');
+	const itemTitle = document.createElement('input');
+	const itemDescriotion = document.createElement('input');
+	const buttonContainer = document.createElement('div');
+	const cancelButton = document.createElement('button');
+	const saveButton = document.createElement('button');
+	const statusElement = document.createElement('span');
+
+	itemContainer.className = 'js-item';
+	itemContainer.appendChild(itemTitle);
+	itemContainer.appendChild(itemDescriotion);
+	itemContainer.appendChild(buttonContainer);
+	itemContainer.appendChild(statusElement);
+
+	itemTitle.className = 'titleOfTheItem';
+	itemTitle.setAttribute('value', item.title)
+	itemTitle.type = 'text';
+	itemTitle.required = 'true';
+
+	itemDescriotion.className = 'descriptionOfTheItem';
+	itemDescriotion.setAttribute('value', item.description)
+	itemDescriotion.type = 'text';
+	itemDescriotion.required = 'true';
+
+	buttonContainer.className = 'button-container';
+	buttonContainer.appendChild(cancelButton);
+	buttonContainer.appendChild(saveButton);
+
+	cancelButton.classList.add('js-cancel-button', 'item-functional-edit-button');
+	cancelButton.innerText = 'Cancel';
+	cancelButton.id = `cancel-${id}`;
+
+	saveButton.classList.add('js-save-button', 'item-functional-edit-button');
+	saveButton.innerText = 'Save';
+	saveButton.id = `save-${id}`;
 
 	statusElement.className = 'status-element';
 	statusElement.innerText = StatusEnam[item.status];
@@ -145,17 +219,38 @@ const initializeEventListeners = () => {
 	}
 
 	const doneButtonItems = document.querySelectorAll('.js-done-button');
-	if(doneButtonItems){
+	if (doneButtonItems) {
 		doneButtonItems.forEach(item => {
 			item.addEventListener('click', onDoneButtonClick)
 		})
 	}
 
 	const holdButtonItems = document.querySelectorAll('.js-hold-button');
-	if(holdButtonItems){
+	if (holdButtonItems) {
 		holdButtonItems.forEach(item => {
 			item.addEventListener('click', onHoldButtonClick)
 		})
+	}
+
+	const editButtonItems = document.querySelectorAll('.js-edit-button');
+	if(editButtonItems){
+		editButtonItems.forEach(item => {
+			item.addEventListener('click', onEditButtonClick)
+		});
+	}
+
+	const cancelButtonItems = document.querySelectorAll('.js-cancel-button');
+	if(cancelButtonItems){
+		cancelButtonItems.forEach(item => {
+			item.addEventListener('click', onEditButtonClick)
+		});
+	}
+
+	const saveButtonItems = document.querySelectorAll('.js-save-button');
+	if(saveButtonItems){
+		saveButtonItems.forEach(item => {
+			item.addEventListener('click', onSaveButtonClick)
+		});
 	}
 }
 
@@ -171,7 +266,7 @@ const onDeleteButtonClick = (event) => {
 const onDoneButtonClick = (event) => {
 	// debugger;
 	const item = event.target.id;
-	const index = item.split('-').pop(); 
+	const index = item.split('-').pop();
 	itemList[index].status = StatusEnam.done
 	renderItemList(itemList)
 	initializeEventListeners()
@@ -179,10 +274,48 @@ const onDoneButtonClick = (event) => {
 
 const onHoldButtonClick = (event) => {
 	const item = event.target.id;
-	const index = item.split('-').pop(); 
-	itemList[index].status = StatusEnam.hold
+	const index = item.split('-').pop();
+
+	if (itemList[index].status === StatusEnam.hold) {
+		itemList[index].status = StatusEnam.pending;
+	} else {
+		itemList[index].status = StatusEnam.hold;
+	}
 	renderItemList(itemList)
 	initializeEventListeners()
+}
+
+const onEditButtonClick = (event) => {
+	const item = event.target.id;
+	const index = item.split('-').pop();
+
+	itemList[index].isEditing = true;
+	renderItemList(itemList)
+	initializeEventListeners()
+
+}
+
+const onCalcelButtonClick = (event) =>{
+	const item = event.target.id;
+	const index = item.split('-').pop();
+
+	itemList[index].isEditing = false;
+	renderItemList(itemList)
+	initializeEventListeners()
+}
+
+const onSaveButtonClick = (event) => {
+	// debugger;
+	const item = event.target.id;
+	const index = item.split('-').pop();
+
+	itemList[index].isEditing = false;
+	itemList[index].title = event.target.parentNode.parentNode.childNodes[0].value;
+	itemList[index].description = event.target.parentNode.parentNode.childNodes[1].value;
+
+	renderItemList(itemList)
+	initializeEventListeners()
+
 }
 
 const onSearchByTitle = (event) => {
@@ -207,20 +340,15 @@ class Item {
 		this.title = title;
 		this.description = description;
 		this.status = StatusEnam.pending;
+		this.isEditing = false;
 	}
 }
 
 const StatusEnam = {
-	'pending' : 'pending' ,
-	'hold' : 'hold',
-	'done' : 'done'
+	'pending': 'pending',
+	'hold': 'hold',
+	'done': 'done'
 }
 Object.freeze(StatusEnam);
 
-
-// const status = new Enum(
-// 	'pending',
-// 	'hold',
-// 	'done'
-// )
 
